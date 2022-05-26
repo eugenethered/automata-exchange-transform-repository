95ee45fcbe42f7fef61360db15507605e7fed502
Eugene the Red <me@eugenethered.com>
2022-05-26 12:02:20 +0100

    update deps

--- exchangetransformrepo/repository/serialize/exchange_transform_deserializer.py
+++ exchangetransformrepo/repository/serialize/exchange_transform_deserializer.py
@@ -4,10 +4,11 @@ from exchangetransformrepo.ExchangeTransform import ExchangeTransform
 
 
 def deserialize_exchange_transform(exchange_transform) -> ExchangeTransform:
-    instrument = as_data(exchange_transform, 'instrument')
-    transform = as_data(exchange_transform, 'transform')
-    ignore = as_data(exchange_transform, 'ignore', False)
-    deserialized = ExchangeTransform(instrument, transform)
-    if ignore:
-        deserialized.ignore = ignore
-    return deserialized
+    if exchange_transform is not None:
+        instrument = as_data(exchange_transform, 'instrument')
+        transform = as_data(exchange_transform, 'transform')
+        ignore = as_data(exchange_transform, 'ignore', False)
+        deserialized = ExchangeTransform(instrument, transform)
+        if ignore:
+            deserialized.ignore = ignore
+        return deserialized
